1c9a90f8d4874318fa86fe449aebd91b4e9e3768
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-05-17 16:19:17 -0300

    improvements: - gitignore - find in subfolders - ability to specify the folder to search for

--- server.rb
+++ server.rb
@@ -5,9 +5,17 @@ require 'haml'
 set :bind, '0.0.0.0'
 
 KNOW_TYPES=%w(png jpg jpeg)
+IMGFOLDER=ENV['IMGFOLDER']
+
+unless IMGFOLDER
+  raise Exception.new("IMGFOLDER Environment variable is not set")
+end
+unless File.exist?(IMGFOLDER)
+  raise Exception.new("'#{IMGFOLDER}' does not exists.")
+end
 
 get '/' do
-  @images = KNOW_TYPES.map {|t| Dir.glob("./public/images/*.#{t}") }.flatten.
+  @images = KNOW_TYPES.map {|t| Dir.glob("#{IMGFOLDER}/**/*.#{t}") }.flatten.
     map { |i| i.gsub 'public/', '' }
   haml :index, :format => :html5
 end
