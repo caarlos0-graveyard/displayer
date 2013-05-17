47245a8854b340cda3b5fa6f2d3226be3f859478
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-05-17 18:17:27 -0300

    fixing img folder specification

--- server.rb
+++ server.rb
@@ -2,11 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'haml'
+
 set :bind, '0.0.0.0'
+set :environment, :production
 
 KNOW_TYPES=%w(png jpg jpeg)
 IMGFOLDER=ENV['IMGFOLDER']
-
 unless IMGFOLDER
   raise Exception.new("IMGFOLDER Environment variable is not set")
 end
@@ -16,6 +17,12 @@ end
 
 get '/' do
   @images = KNOW_TYPES.map {|t| Dir.glob("#{IMGFOLDER}/**/*.#{t}") }.flatten.
-    map { |i| i.gsub 'public/', '' }
+    map { |i| "img/#{i.gsub(IMGFOLDER, '')}" }
   haml :index, :format => :html5
+end
+
+get '/img/:i' do
+  filename = "#{IMGFOLDER}/#{params[:i]}"
+  puts "FILENAME #{filename}"
+  send_file(filename)
 end
